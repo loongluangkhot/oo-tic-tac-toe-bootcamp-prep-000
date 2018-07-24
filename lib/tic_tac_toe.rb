873940f1d72c6e75ad328e931fd1d8ccbe9781ee
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-24 16:53:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,8 +29,8 @@ class TicTacToe
     user_input.to_i - 1
   end
   
-  def move
-    @board[@index] = @current_player
+  def move(index, current_player)
+    @board[index] = current_player
   end
   
   # def position_taken?(board, location)
